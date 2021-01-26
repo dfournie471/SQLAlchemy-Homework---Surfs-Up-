b93914c88633ce56ef5ea46d317e8b549cd8e6bf
David Fournie <drfournie@gmail.com>
2021-01-25 22:17:15 -0600

    added final files and removed bonuses which I didnt have time to finish

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SQLAlchemy Homework - Surfs-Up
-week 10 homework
+### David Fournie
+#### 1/25/2021
+
+####
+### Step 1 - Climate Analysis and Exploration
+Using hawaii.sqlite as a base for queries, we found that 2017-08-23 was the final date in the database. Since we wanted to look at the precipitation for the final 12 months of the dataframe, our starting point was determined to be 2016-08-23. By selecting the date and prcp values from hawaii.squlite we were able to plot the data as a bargraph:
+![Precipitation Measurements](output/Precipitation_Measurements.png)
+ ####
+ This let us see amounts of precipitation throughout the final year. All in all the rain followed typical patterns for an area that has rainy seasons. The late summer/early fall, and spring values were higher than their winter & summer counterparts. One unintended conclusion of the data is that if you hate rain, the time to visit Hawaii is in the non-rainy seasons.
+ ###
+The next phase of the climate analysis step was to find which weather station in hawaii.sqlite had the most observations. USC00519281 AKA WAIHEE 837.5, HI US was the most active station in our data set. Looking at the temperature data  for USC00519281 in the last recorded year revealed that the max temp was 85 degrees, the min temp was 54 degrees, and the avg temp was 72 degrees. Plotting the data in a histogram revealed the most frequent temperatures occured in the low to mid 70's which fits with the measured avg temp.
+![Temperature Histogram](output/Temperature_Histogram.png)
+
+####
+### Step 2 - Climate App
+We created a companion App that breaks the data down into lists and dictionaries.
+in order to access the app, run  app.py in vs code. This will grant access to a development server. clicking on the local link provided will take you to the home page. Here is an example: http://127.0.0.1:5000/.
+
+The first three links available provide access to precipitation, station, and temperature data. In order to access these links, simply copy and paste the link text on the homepage to the end of the home url. For instance, if you wanted to access precipitation data for station USC00519281 , the link would look like this: http://127.0.0.1:5000/api/v1.0/precipitation
+
+The first three links work the same as the example above.
+
+The last two links allow for user input of a date in the dataframe of their choosing. In order to accomplish this replace the START and or END in the links with the date of your choice. The date(s) must be in the yyyy-mm-dd format and the date range is from 2010-01-01 to 2017-08-23. 
+
+Example for one date: http://127.0.0.1:5000/api/v1.0/2015-09-16 
+This returns the avg temp, max temp, and min temp, the all the temps collected from 2015-09-16 to the end of the database.
+
+Example for date range:http://127.0.0.1:5000/api/v1.0/2015-09-16/2016-09-16.
+This returns the avg temp, max temp, and min temp, the all the temps collected between 2015-09-16 and 2016-09-16 inclusive. Happy exploring!
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#import dependencies
 import numpy as np
 import datetime as dt
 import sqlalchemy
@@ -6,6 +7,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+
+
 
 #database setup
 
@@ -16,38 +19,53 @@ Base.prepare(engine,reflect=True)
 Measurement=Base.classes.measurement
 Station=Base.classes.station
 
+#setting global variables for date list
+session=Session(engine)
+results_1=session.query(Measurement.date).distinct().\
+        order_by(Measurement.date).all()
+session.close()
+dates_list=list(np.ravel(results_1))
+
+
+#define flask app
 app=Flask(__name__)
 
+#set 'Home' route
 @app.route("/")
+#create function that displays instructions for the pathways
 def welcome():
     print("Server has recieved request for 'Welcome' page...")
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tob<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/START<br/>"
+        f"/api/v1.0/START/END"
     )
 
+#set precipitation route
 @app.route("/api/v1.0/precipitation")
+# create function that returns precipitation results for query
 def precipitation():
     session=Session(engine)
     
     
-    results=session.query(Measurement.date,func.round(func.sum(Measurement.prcp),2)).\
-        group_by(Measurement.date).all()
+    #filter query by station with most measurments
+    results=session.query(Measurement.date,Measurement.prcp).\
+        filter(Measurement.station=='USC00519281').all()
+     #close session for efficiency   
     session.close()
     
     
-    all_dates=[]
+    #create dictionary that uses date as key and precipitation as value
+    precipitation_dict={}
     for date, precipitation in results:
-        precipitation_dict={}
-        precipitation_dict["date"] = date
-        precipitation_dict["precipitation"] = precipitation
-        all_dates.append(precipitation_dict)
-    
-    return jsonify(all_dates)
+        
+        precipitation_dict[date] = precipitation
+        
+        #return jsonified dictionary
+    return jsonify(precipitation_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -55,19 +73,91 @@ def stations():
     results=session.query(Station.station,Station.name).all()
     session.close()
 
-    all_stations=[]
+    station_dict={}
     for station_id, station_name in results:
-        station_dict={}
-        station_dict["station_id"]=station_id
-        station_dict["station_name"]=station_name
-        all_stations.append(station_dict)
-    return jsonify(all_stations)
+        station_dict[station_id]=station_name
+        
+    return jsonify(station_dict)
+# set route for tobs
+@app.route("/api/v1.0/tobs")
+#same repeated process as the precipatition route
+def temperature():
+    session=Session(engine)
+    results = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >='2016-08-23').\
+        filter(Measurement.date <='2017-08-23').all()
+    session.close()
+    popular_dict={}
+    
+    for date, measurement_tobs in results:
+        popular_dict[date]=measurement_tobs
+    return jsonify(popular_dict)
+
+#set route for start date only
+@app.route("/api/v1.0/<start>")
+def one_date(start):
+    #created variable to store the string of start
+    canonicalization=f'{start}'
+    
+    #check to see if date entered in url is valid
+    if start in dates_list:
+        #create session
+        session=Session(engine)
+        
+        #create query saved to results
+        results=session.query(func.min(Measurement.tobs),func.round(func.avg(Measurement.tobs),2),func.max(Measurement.tobs)).\
+            filter(Measurement.date >= canonicalization).\
+            order_by(Measurement.date).all()
+        session.close()
+        
+        #create list of dictionaries that will display query
+        #testing a few aestetics for data display
+        #create blank list
+        all_temp=[]
+        #loop through results query and save elements to dictionary
+        for temp_min,temp_avg,temp_max in results:
+            temp_dict={}
+            temp_dict["Min Temp"]=temp_min
+            temp_dict["Average Temp"]=temp_avg
+            temp_dict["Max Temp"]=temp_max
+            #append dictionaries to all_temp
+            all_temp.append(temp_dict)
+        
+        #return jsonify of all_temp
+        return jsonify(all_temp)
+    #if date not found return else
+    else:
+        return f"error! date: {start} not found",404
 
 
 
+#repeat procoss of start date to date range
+@app.route("/api/v1.0/<start>/<end>")
+def date_range(start=None,end=None):
+    
+    if start in dates_list and end in dates_list and end > start:
+        session=Session(engine)
+        results_2=session.query(func.min(Measurement.tobs),
+                            func.round(func.avg(Measurement.tobs),2),
+                            func.max(Measurement.tobs)).\
+                            filter(Measurement.date >= start).\
+                            filter(Measurement.date<= end).all()
+    
+        session.close()
+        all_temp_2=[]
+        for temp_min_2,temp_avg_2,temp_max_2 in results_2:
+            temp_dict_2={}
+            temp_dict_2["Min Temp"]=temp_min_2
+            temp_dict_2["Average Temp"]=temp_avg_2
+            temp_dict_2["Max Temp"]=temp_max_2
+            all_temp_2.append(temp_dict_2)
+        
+        return jsonify(all_temp_2)
+    else:
+        return f"error! date range: {start} : {end} not found",404
 
 
-
-
+#close out flask
 if __name__=='__main__':
     app.run(debug=True)
